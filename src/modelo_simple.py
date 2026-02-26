95e5dfd65c682c45cf56f1890add7e4b090c72a0
juango9 <juango9@alumni.uv.es>
2026-02-26 10:23:03 +0100

    Mejora de la explicacion del modelo

--- src/modelo_simple.py
+++ src/modelo_simple.py
@@ -1,9 +1,14 @@
 """
 Modelo de red neuronal mas simple posible para la clasificacion de basura.
+
 Este modelo constara de una sola capa oculata convolucional donde el numero de neuronas sera igual
 al numero de clases, que tiene el conjunto de datos un total de 6 clases.
-Luego se realizara un GlobalAvaregePooling o un GlobalMazPooling para asi resucir a un vector
-de 6 elemento es decir igual a la clases que tenemos.
+
+Una vez aplicada la funcion de activacion Relu se realizara un GlobalAvaregePooling 
+o un GlobalMaxPooling para asi reducir cada una  de las matrices resultantes de 126x126 
+matriz de 1x1, dando como resultado final con las 6 neuronas un vector de 1x6.
+
+Luego se aplicara una capa densa con la funcion de activacion softmax para obtener las probabilidades de cada clase y asi realizar la clasificacion.
 """
 
 from keras.models import Sequential
