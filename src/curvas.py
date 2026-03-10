e245cbdead1aa68b5045ab6b5064267b4ba2af93
juango9 <juango9@alumni.uv.es>
2026-03-10 13:36:20 +0100

    Añadido la funcion para grafico matriz confusion

--- src/curvas.py
+++ src/curvas.py
@@ -1,5 +1,7 @@
 
 import matplotlib.pyplot as plt
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+import numpy as np
 
 def graf_pedida(hist):
    plt.plot(hist.history['loss'],label="loss")
@@ -14,3 +16,16 @@ def graf_acc(hist,):
     plt.legend()
     grafico=plt.show()
     return grafico
+
+def plot_confusion(y_true, y_pred, class_names, title='Matriz de confusion'):
+    # Redondear predicciones a etiquetas enteras
+    y_pred_labels = np.rint(y_pred).astype(int)
+    y_pred_labels = np.clip(y_pred_labels, 0, len(class_names) - 1)
+
+    labels = list(range(len(class_names)))
+    cm = confusion_matrix(y_true, y_pred_labels, labels=labels)
+    disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=class_names)
+    disp.plot(cmap='Blues')
+    plt.title(title)
+    plt.tight_layout()
+    plt.show()
